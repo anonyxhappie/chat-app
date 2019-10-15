bb417e9e03b8549ddcc40359f31c23f0e817d207
Akshay Saini <akshay.saini@quantiphi.com>
2019-10-15 18:39:52 +0530

    improving system with fixes & updates

--- chat-ui/src/app/app.component.ts
+++ chat-ui/src/app/app.component.ts
@@ -24,29 +24,31 @@ export class AppComponent {
   title = 'chat-ui';
   messages: Message[] = [];
   users: User[] = [];
-  currentUser: User;
+  currentUser: User = { full_name: 'Unknown', user_id: '', user_name: '' };
   otherUser: User;
   message: string;
   websocket: any;
   port = '6789';
 
   constructor(private http: HttpClient) {
+
     this.websocket = new WebSocket('ws://localhost:' + this.port + '/');
+    this.websocket.onopen = () => this.websocket.send({ action: 'plus' });
+    console.log('trt', this.websocket);
     // this.websocket.send(JSON.stringify({action: 'plus'}));
     this.onWebsockectsMessageReceived();
     this.getUsers();
-    this.getMessages();
-  }
-
-  setWebSocket() {
-    this.websocket = new WebSocket('ws://localhost:' + this.port + '/');
   }
 
   getMessages() {
-    return this.http.get('http://localhost:8000/messages/').subscribe((data: any) => {
-      this.messages = [...data];
-      console.log(this.messages);
-    });
+    return this.http.get(
+      'http://localhost:8000/text?from=' +
+      this.currentUser.user_id +
+      '&to=' +
+      this.otherUser.user_id).subscribe((data: any) => {
+        this.messages = [...data];
+        // console.log(this.messages);
+      });
   }
 
   sendMessage() {
@@ -55,14 +57,9 @@ export class AppComponent {
       sent_to: this.otherUser.user_id,
       message: this.message
     };
-    console.log(body);
-    this.websocket.send(JSON.stringify({action: 'plus'}));
-    return this.http.post('http://localhost:8000/text', {
-      sent_from: this.currentUser.user_id,
-      sent_to: this.otherUser.user_id,
-      message: this.message
-    }).subscribe((response: any) => {
-      console.log(response);
+    this.websocket.send(JSON.stringify({ action: 'message' }));
+    return this.http.post('http://localhost:8000/text', body).subscribe((response: any) => {
+      // console.log(response);
       this.message = '';
     });
   }
@@ -72,9 +69,7 @@ export class AppComponent {
       this.users = data;
       this.currentUser = this.users[0];
       this.otherUser = this.users[1];
-      console.log(this.users);
-      console.log('from:', this.currentUser.user_name);
-      console.log('to:', this.otherUser.user_name);
+      this.getMessages();
     });
   }
 
@@ -85,19 +80,44 @@ export class AppComponent {
     return u[0].user_name;
   }
 
+  switchUsers() {
+    const tempUser = { ...this.currentUser };
+    this.currentUser = { ...this.otherUser };
+    this.otherUser = { ...tempUser };
+  }
+
   onCurrentUserChange() {
-    this.otherUser = this.users.filter(user => {
-      return user.user_id !== this.currentUser.user_id;
-    })[0];
-    console.log('from:', this.currentUser.user_name);
-    console.log('to:', this.otherUser.user_name);
+    this.getMessages();
+  }
+
+  onOtherUserChange() {
+    this.getMessages();
   }
 
   onWebsockectsMessageReceived() {
+    this.websocket.onopen = (event) => {
+      console.log('On websocket open');
+      console.log('::', event);
+    };
     this.websocket.onmessage = (event) => {
       const data = JSON.parse(event.data);
-      console.log(data);
+      console.log('On websocket message');
+      console.log('::', event);
+      // console.log('onWebsockectsMessageReceived::', data);
+      // console.log(data['type']);
+      if (data['type'] === 'users') {
+        return;
+      }
       this.messages = [...this.messages, data];
     };
+    this.websocket.onerror = (event) => {
+      console.log('On websocket error');
+      console.log('::', event);
+    };
+    this.websocket.onclose = (event) => {
+      console.log('On websocket close');
+      console.log('::', event);
+    };
   }
+
 }
--- chatServer/chat_api/views.py
+++ chatServer/chat_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
 from .serializers import ChatUser, Message, Notification, ChatUserSerializer, MessageSerializer, NotificationSerializer
 from logpipe import Producer
 from chatServer.settings import KAFKA_PRODUCER as kfk_producer
@@ -23,6 +24,19 @@ class NotificationModelViewSet(ModelViewSet):
 
 
 class MessageController(APIView):
+
+    def get(self, request):
+        try:
+            user1 = ChatUser.objects.get(user_id=request.GET.get('from'))
+            user2 = ChatUser.objects.get(user_id=request.GET.get('to'))
+            m = Message.objects.filter(Q(Q(sent_from=user1) & Q(sent_to=user2)) | Q(Q(sent_from=user2) & Q(sent_to=user1))).order_by('created_at')
+            serializer = MessageSerializer(m, many=True)      
+            output = serializer.data
+            return Response(output)
+        except KafkaError as e:
+            return Response({'error': str(e)})
+        except Exception as e:
+            return Response({'error': str(e)})
 
     def post(self, request):
         try:
